3f99edf031e1c3bd2c7db76f5ba7362e63fe4b97
Lanre Omidiran <lanreomidiran@gmail.com>
2026-04-01 14:51:02 +0100

    Admin Dashboard

--- AdminDashboard_UI/Models/Users.cs
+++ AdminDashboard_UI/Models/Users.cs
@@ -1,15 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AdminDashboard_UI.Models
 {
+    [Table("Users")]
     public class Users
     {
         [Key]
         public int UserId { get; set; }
         [MaxLength(40)]
         public string? Name { get; set; }
-        [EmailAddress]
-        public string Email { get; set; }
-        public string Role { get; set; }
+        [MaxLength(40)]
+        public string? Email { get; set; }
+        [MaxLength(20)]
+        public string? Role { get; set; }
     }
 }
